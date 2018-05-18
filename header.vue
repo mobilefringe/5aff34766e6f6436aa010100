b93b7adfde50292125a620d21ab9f2e6c29dd2d4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-18 17:03:54 -0400

    Southcentre Mall 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -10,12 +10,7 @@
     					<nav id="primary_nav">
     						<ul>
     						    <li v-for="item in menu_items" class="menu_item">
-    						        <router-link :to="item.href">{{$t(item.name)}}</router-link>
-    						        <ul v-if="item.sub_menu">
-    						            <li v-for="sub_menu in item.sub_menu" class="dropdown_item">
-    						                <router-link :to="sub_menu.href">{{$t(sub_menu.name)}}</router-link>
-    						            </li>
-    								</ul>
+    						        <div class="site_map_cell ss"></div><router-link :to="item.href">{{$t(item.name)}}</router-link></div>
     						    </li>
     						</ul>
     					</nav>
