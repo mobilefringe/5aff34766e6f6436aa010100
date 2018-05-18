9519afd11310d0b2ad2b35039182411aa892c582
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-18 17:07:35 -0400

    Southcentre Mall 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -1,24 +1,243 @@
 <template>
-    <div class="site_menu">
-    	<div class="menu_bar hidden_phone">
-    		<div class="site_container">
-    			<div class="nav_container hidden_phone">
-    				<div class="site_logo">
-    					<router-link to="/"><img :src="property_logo" alt="Property Logo"/></router-link>
-    				</div>
-    				<div class="row">
-    					<nav>
-    						<ul>
-    						    <li v-for="item in menu_items">
-    						        <div class="site_map_cell ss"><router-link :to="item.href">{{$t(item.name)}}</router-link></div>
-    						    </li>
-    						</ul>
-    					</nav>
-    				</div>
-    			</div>
-    		</div>
-    	</div>
-    </div>
+    <!--<div class="site_menu">-->
+    <!--	<div class="menu_bar hidden_phone">-->
+    <!--		<div class="site_container">-->
+    <!--			<div class="nav_container hidden_phone">-->
+    <!--				<div class="site_logo">-->
+    <!--					<router-link to="/"><img :src="property_logo" alt="Property Logo"/></router-link>-->
+    <!--				</div>-->
+    <!--				<div class="row">-->
+    <!--					<nav>-->
+    <!--						<ul>-->
+    <!--						    <li v-for="item in menu_items">-->
+    <!--						        <div class="site_map_cell ss"><router-link :to="item.href">{{$t(item.name)}}</router-link></div>-->
+    <!--						    </li>-->
+    <!--						</ul>-->
+    <!--					</nav>-->
+    <!--				</div>-->
+    <!--			</div>-->
+    <!--		</div>-->
+    <!--	</div>-->
+    <!--</div>-->
+    <nav class="top_menu" data-topbar="" role="navigation">
+                <div class="mobile_topbar">
+                  <div class="small-3 columns">
+                    <div class="mobile_menu_btn">
+                      <img alt="menu" src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/003/853/original/icon_menu.png?1410203768">
+                    </div>
+                  </div>
+                  <div class="small-6 columns">
+                    <a href="/"><img alt="logo" src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/003/854/original/logo_sc_mobile.png?1410203796"></a>
+                  </div>
+                  <div class="small-3 columns">
+                    <div class="search_icon" style="user-select: none; touch-action: pan-y; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);">
+                      <img alt="search mobile" src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/003/855/original/icon_search_mobile.png?1410203827">
+                    </div>
+                  </div>
+                </div>
+                <div class="site_menu" style="display: block;">
+                  <ul>
+                    <li>
+                      <div class="site_map_cell">
+                        <a href="/"><img style="max-width:170px" alt="logo" src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/016/872/original/logo_sc(desktop)_(1).png?1455645887"></a>
+                      </div>
+                    </li>
+                    <li>
+                      <div class="site_map_cell ss">
+                        <a href="#hours">HOURS</a>
+                      </div>
+                      <div class="site_map_cell mobile">
+                        <div class="small-10 columns">
+                          <a href="#hours">HOURS</a>
+                        </div>
+                        <div class="small-2 columns">
+                          <img alt="mobile menu" src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/003/857/original/mobile_menu_arrow.png?1410203882">
+                        </div>
+                      </div>
+                    </li>
+                    <div class="hours_divider"></div>
+                
+           
+                   
+                   
+                    <li>
+                      <div class="site_map_cell ss">
+                        <a href="#directory">DIRECTORY</a>
+                      </div>
+                      <div class="site_map_cell mobile">
+                        <div class="small-10 columns">
+                          <a href="#directory">DIRECTORY</a>
+                        </div>
+                        <div class="small-2 columns">
+                          <img alt="menu" src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/003/857/original/mobile_menu_arrow.png?1410203882">
+                        </div>
+                      </div>
+                    </li>
+                    <div class="hours_divider"></div>
+                    
+                    
+                
+                         
+
+
+
+                    
+                
+
+
+
+                
+                    <li>
+                      <div class="site_map_cell ss">
+                        <a href="#promotions">STORE PROMOTIONS</a>
+                      </div>
+                      <div class="site_map_cell mobile">
+                        <div class="small-10 columns">
+                          <a href="#promotions">STORE PROMOTIONS</a>
+                        </div>
+                        <div class="small-2 columns">
+                          <img alt="menu arrow" src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/003/857/original/mobile_menu_arrow.png?1410203882">
+                        </div>
+                      </div>
+                    </li>
+                    <div class="hours_divider"></div>
+                    <li>
+                      <div class="site_map_cell ss">
+                        <a href="#style">STYLE ACADEMY</a>
+                      </div>
+                      <div class="site_map_cell mobile">
+                        <div class="small-10 columns">
+                          <a href="#style">STYLE ACADEMY</a>
+                        </div>
+                        <div class="small-2 columns">
+                          <img alt="arrow" src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/003/857/original/mobile_menu_arrow.png?1410203882">
+                        </div>
+                      </div>
+                    </li>
+                    <div class="hours_divider"></div>
+                    <li>
+                      <div class="site_map_cell ss">
+                        <a href="#giftcard">GIFT CARDS</a>
+                      </div>
+                      <div class="site_map_cell mobile">
+                        <div class="small-10 columns">
+                          <a href="#giftcard">GIFT CARDS</a>
+                        </div>
+                        <div class="small-2 columns">
+                          <img alt="menu" src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/003/857/original/mobile_menu_arrow.png?1410203882">
+                        </div>
+                      </div>
+                    </li>
+                    <div class="hours_divider"></div>
+                    <li>
+                      <div class="site_map_cell ss">
+                        <a href="#kidsfun">KIDS FUN</a>
+                      </div>
+                      <div class="site_map_cell mobile">
+                        <div class="small-10 columns">
+                          <a href="#kidsfun">KIDS FUN</a>
+                        </div>
+                        <div class="small-2 columns">
+                          <img alt="menu arrow" src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/003/857/original/mobile_menu_arrow.png?1410203882">
+                        </div>
+                      </div>
+                    </li>
+                    <div class="hours_divider"></div>
+                    <li>
+                      <div class="site_map_cell ss">
+                        <a href="#news_and_events">NEWS &amp; EVENTS</a>
+                      </div>
+                      <div class="site_map_cell mobile">
+                        <div class="small-10 columns">
+                          <a href="#news_and_events">NEWS &amp; EVENTS</a>
+                        </div>
+                        <div class="small-2 columns">
+                          <img alt="menu" src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/003/857/original/mobile_menu_arrow.png?1410203882">
+                        </div>
+                      </div>
+                    </li>
+                    <div class="hours_divider"></div>
+                    <li class="upper-links-mobile">
+                        <div></div>
+                        <div class="site_map_cell mobile">
+                            <div class="small-10 columns">
+                                <a href="/pages/southcentre-guest-services">GUEST SERVICES</a>
+                            </div>
+                            <div class="small-2 columns">
+                                <img alt="menu" src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/003/857/original/mobile_menu_arrow.png?1410203882">
+                            </div>
+                        </div>
+                    </li>
+                 <div class="hours_divider"></div>
+                    <li class="upper-links-mobile">
+                        <div></div>
+                        <div class="site_map_cell mobile">
+                            <div class="small-10 columns">
+                                <a href="/pages/southcentre-green-initiatives">GREEN INITIATIVES</a>
+                            </div>
+                            <div class="small-2 columns">
+                                <img alt="menu" src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/003/857/original/mobile_menu_arrow.png?1410203882">
+                            </div>
+                        </div>
+                    </li>
+         
+                
+                    <div class="hours_divider"></div>
+                    <li class="upper-links-mobile">
+                        <div></div>
+                        <div class="site_map_cell mobile">
+                            <div class="small-10 columns">
+                                <a href="/pages/southcentre-contact">CONTACT</a>
+                            </div>
+                            <div class="small-2 columns">
+                                <img alt="menu" src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/003/857/original/mobile_menu_arrow.png?1410203882">
+                            </div>
+                        </div>
+                    </li>
+                    <div class="hours_divider"></div>
+                    <li class="upper-links-mobile">
+                        <div></div>
+                        <div class="site_map_cell mobile">
+                            <div class="small-10 columns">
+                                <a href="/pages/southcentre-leasing">LEASING</a>
+                            </div>
+                            <div class="small-2 columns">
+                                <img alt="menu" src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/003/857/original/mobile_menu_arrow.png?1410203882">
+                            </div>
+                        </div>
+                    </li>
+                    <div class="hours_divider"></div>
+                    <li class="">
+                        <div></div>
+                        <div class="site_map_cell mobile">
+                            <div class="small-10 columns">
+                                <a href="/jobs">CAREERS</a>
+                            </div>
+                            <div class="small-2 columns">
+                                <img alt="menu" src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/003/857/original/mobile_menu_arrow.png?1410203882">
+                            </div>
+                        </div>
+                    </li>
+                  
+                   
+                    <div class="hours_divider"></div>
+                    <li class="search_icon" style="user-select: none; touch-action: pan-y; -webkit-user-drag: none; -webkit-tap-highlight-color: rgba(0, 0, 0, 0);">
+                      <div class="site_map_cell">
+                        <img alt="menu" src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/003/858/original/icon_search.png?1410203942">
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+                <div class="search_bar hidden" style="display: none;">
+                    <div class="search_bar_wrap">
+                        <form id="search_form" action="/search" method="GET">
+                            <div class="row">
+                                <input class="SearchTerms_phone" name="query" id="SearchTerms" type="search" placeholder="SEARCH" title="search">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+              </nav>
 </template>
 
 <script>
