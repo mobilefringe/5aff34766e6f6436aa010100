d32949977ee9e6c8a5b8b5129b8d031d2c62bcb5
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-22 12:40:24 -0400

    Southcentre Mall 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -46,7 +46,7 @@
 </template>
 
 <script>
-    define(["Vue", "vuex", "vue!today_hours", "vue!search-component", 'vue!vue-slick', 'js-cookie', 'masonry' , 'vue-masonry-plugin', 'vue-lazy-load'], function(Vue, Vuex, TodayHoursComponent, SearchComponent, slick, Cookies, masonry, VueMasonryPlugin, VueLazyload) {
+    define(["Vue", "vuex", "vue!search-component", 'vue!vue-slick', 'js-cookie', 'masonry' , 'vue-masonry-plugin', 'vue-lazy-load'], function(Vue, Vuex, SearchComponent, slick, Cookies, masonry, VueMasonryPlugin, VueLazyload) {
         Vue.use(VueMasonryPlugin.default);
         Vue.use(VueLazyload);
         return Vue.component("home-component", {
