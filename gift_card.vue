53320b40ac88a2736bf9820a3b4ce43d99fa8ee4
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-25 12:50:51 -0400

    Southcentre Mall 1.0  /gift_card.vue

--- gift_card.vue
+++ gift_card.vue
@@ -1,48 +1,46 @@
 <template>
-    <section id="giftcard" class="scrollspy">
-            <div class="section_content">
-              <div class="giftcard_background">
-                <div class="wrap-90">
-                  <div class="giftcard_wrap position_relative">
-                  <div class="gc_centres_popup">
-                    <span id="close_gc_centre">X</span>
-                    <img src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/015/733/original/SOM_188_GCPlus_Website_Update_V2B.jpg?1450716958" />
-                  </div>
-                    <div class="giftcard_content">
-                      <h1 class="white">ONE CARD</h1>
-                      <h1 class="white">SO MANY POSSIBILITIES</h1>
-                      <br/>
-                      <p class="freedom whitney">SHOP. STAY. FLY.</p>
-                      <div class="row giftcard-description">
-                        <div class="giftcard_left medium-7 columns">
-                          <p class="whitney-medium all_caps">Introducing the new Southcentre Gift Card Plus!</p>
-                          <p class="whitney">
-                            We’ve added new benefits to our gift cards. The Southcentre Gift Card Plus is redeemable at <span class="whitney-medium gc_mobile">our centre</span><span class="whitney-medium gc_desktop" >our centre</span>, or any one of our <span class="whitney-medium"><a target="_blank" href="https://oxfordgiftcardplus.ca/where-to-use">Oxford Properties</a></span>, or can be used to book a hotel stay, or fly anywhere Porter Airlines flies.
-                          </p>
-                          <p class="whitney">
-                            Purchase your Southcentre Gift Card Plus at Guest Services or order <span class="whitney-medium"><a target="_blank" href="https://oxfordgiftcardplus.ca/buy-gift-card/">online</a></span> and enjoy the convenience of having your gift card delivered to your door.
-                          </p>
-                          <p class="whitney">
-                            For corporate orders please email:<br />
-                          
-                            info@southcentremall.com for futher details.
-                          </p>
-                          <p class="whitney gc_links">
-                            <a href="https://oxfordgiftcardplus.ca/" target="_blank">CORPORATE ORDERS</a><br />
-                            <a href="https://oxfordgiftcardplus.ca/buy-gift-card/" target="_blank">ONLINE PURCHASES</a><br />
-                            <a href="https://oxfordgiftcardplus.ca/faq/" target="_blank">FAQS</a><br />
-                            <a href="https://www279.americanexpress.com/GPTHBIWeb/validateIPAction.do?clientkey=amex%20bank%20of%20canada" target="_blank">CHECK YOUR BALANCE</a><br />
-                          </p>
-                         
-                        </div>
-                        <div class="giftcard_right medium-5 columns">
-                          <img alt="gift card" src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/015/466/original/gift_card_img.png?1449786330"/>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
+	<section id="giftcard" class="scrollspy">
+		<div class="section_content">
+			<div class="giftcard_background">
+				<div class="wrap-90">
+					<div class="giftcard_wrap position_relative">
+						<div class="gc_centres_popup">
+							<span id="close_gc_centre">X</span>
+							<img src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/015/733/original/SOM_188_GCPlus_Website_Update_V2B.jpg?1450716958" />
+						</div>
+						<div class="giftcard_content">
+							<h1 class="white">ONE CARD</h1>
+							<h1 class="white">SO MANY POSSIBILITIES</h1>
+							<br/>
+							<p class="freedom whitney">SHOP. STAY. FLY.</p>
+							<div class="row giftcard-description">
+								<div class="giftcard_left medium-7 columns">
+									<p class="whitney-medium all_caps">Introducing the new Southcentre Gift Card Plus!</p>
+									<p class="whitney">
+										We’ve added new benefits to our gift cards. The Southcentre Gift Card Plus is redeemable at <span class="whitney-medium gc_mobile">our centre</span><span class="whitney-medium gc_desktop" >our centre</span>, or any one of our <span class="whitney-medium"><a target="_blank" href="https://oxfordgiftcardplus.ca/where-to-use">Oxford Properties</a></span>, or can be used to book a hotel stay, or fly anywhere Porter Airlines flies.
+									</p>
+									<p class="whitney">
+										Purchase your Southcentre Gift Card Plus at Guest Services or order <span class="whitney-medium"><a target="_blank" href="https://oxfordgiftcardplus.ca/buy-gift-card/">online</a></span> and enjoy the convenience of having your gift card delivered to your door.
+									</p>
+									<p class="whitney">
+										For corporate orders please email:<br />
+										info@southcentremall.com for futher details.
+									</p>
+									<p class="whitney gc_links">
+										<a href="https://oxfordgiftcardplus.ca/" target="_blank">CORPORATE ORDERS</a><br />
+										<a href="https://oxfordgiftcardplus.ca/buy-gift-card/" target="_blank">ONLINE PURCHASES</a><br />
+										<a href="https://oxfordgiftcardplus.ca/faq/" target="_blank">FAQS</a><br />
+										<a href="https://www279.americanexpress.com/GPTHBIWeb/validateIPAction.do?clientkey=amex%20bank%20of%20canada" target="_blank">CHECK YOUR BALANCE</a><br />
+									</p>
+								</div>
+								<div class="giftcard_right medium-5 columns">
+									<img alt="gift card" src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/015/466/original/gift_card_img.png?1449786330"/>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 </template>
