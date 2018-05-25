116c96c6b5102fd506790af49089fab099fb9534
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-25 12:12:39 -0400

    Southcentre Mall 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -88,8 +88,8 @@
                             temp_promo.push(value);
                         }
                     });
-                    _.sortBy(temp_promo, [function(o) { return o.end_date; }]).reverse();
-                    return temp_promo;
+                    
+                    return _.sortBy(temp_promo, [function(o) { return o.end_date; }]).reverse();
                 },
             },
             methods: {
