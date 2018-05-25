2369917048ff64076122e3b3c1451c669548deef
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-25 13:03:47 -0400

    Southcentre Mall 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -1,58 +1,35 @@
 <template>
 	<section id="news_and_events" class="scrollspy" v-if="dataloaded">
 		<div class="section_content">
-              <h1>NEWS & EVENTS</h1>
-              <p class="whitney">STAY UP TO DATE AND IN THE KNOW</p>
-              <div class="event_content">
-                <div>
-                  
-                  
-                  
-                  <div class="event_box">
-                      <h3 class="event_title chronicle">Where Fashion Meets Furniture</h3>
-                      
-                      <h3 class="event_date whitney-light">May 01 - May 31</h3>
-                      
-                      <a class="whitney blue" href="/events/southcentre-where-fashion-meets-furniture">More info</a>
-                  </div>
-                  
-                  
-                  
-                  <div class="event_box">
-                      <h3 class="event_title chronicle">Style Academy- Swimwear!</h3>
-                      
-                      <h3 class="event_date whitney-light">May 24</h3>
-                      
-                      <a class="whitney blue" href="/events/southcentre-style-academy-swimwear">More info</a>
-                  </div>
-                  
-                  
-                  
-                  <div class="event_box">
-                      <h3 class="event_title chronicle">Set for Summer Workshops</h3>
-                      
-                      <h3 class="event_date whitney-light">May 25 - May 27</h3>
-                      
-                      <a class="whitney blue" href="/events/southcentre-edible-flower-garden-class">More info</a>
-                  </div>
-                  
-                  
-                  
-                  <div class="event_box">
-                      <h3 class="event_title chronicle">Style Academy - Men's Edition!</h3>
-                      
-                      <h3 class="event_date whitney-light">Jun 12</h3>
-                      
-                      <a class="whitney blue" href="/events/southcentre-style-academy-men-s-edition">More info</a>
-                  </div>
-                  
-                </div>
-                
-              </div>
-            </div>
-          </section>
+			<h1>NEWS & EVENTS</h1>
+			<p class="whitney">STAY UP TO DATE AND IN THE KNOW</p>
+			<div class="event_content">
+				<div>
+					<div class="event_box">
+						<h3 class="event_title chronicle">Where Fashion Meets Furniture</h3>
+						<h3 class="event_date whitney-light">May 01 - May 31</h3>
+						<a class="whitney blue" href="/events/southcentre-where-fashion-meets-furniture">More info</a>
+					</div>
+					<div class="event_box">
+						<h3 class="event_title chronicle">Style Academy- Swimwear!</h3>
+						<h3 class="event_date whitney-light">May 24</h3>
+						<a class="whitney blue" href="/events/southcentre-style-academy-swimwear">More info</a>
+					</div>
+					<div class="event_box">
+						<h3 class="event_title chronicle">Set for Summer Workshops</h3>
+						<h3 class="event_date whitney-light">May 25 - May 27</h3>
+						<a class="whitney blue" href="/events/southcentre-edible-flower-garden-class">More info</a>
+					</div>
+					<div class="event_box">
+						<h3 class="event_title chronicle">Style Academy - Men's Edition!</h3>
+						<h3 class="event_date whitney-light">Jun 12</h3>
+						<a class="whitney blue" href="/events/southcentre-style-academy-men-s-edition">More info</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
 </template>
-
 <style>
     .events_container .date_bar{
         /* Today: */
