ea8f747a91b95313ce7f41b3e38937866c7fe33b
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-25 16:18:32 -0400

    Southcentre Mall 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -10,7 +10,7 @@
 			</div>
 		</div>
 		<header-component></header-component>
-		<header-component v-if=""></header-component>
+		<header-component v-if="showStickyMenu"></header-component>
 		<div class="main_content">
 		    <hours-component></hours-component>
     		<stores-component></stores-component>
