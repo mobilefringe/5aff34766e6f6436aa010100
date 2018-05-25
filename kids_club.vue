6aa66435be72ec933bce2a5d670a3afbdd76648c
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-25 12:59:30 -0400

    Southcentre Mall 1.0  /kids_club.vue

--- kids_club.vue
+++ kids_club.vue
@@ -1,48 +1,66 @@
 <template>
-	<section id="giftcard" class="scrollspy">
-		<div class="section_content">
-			<div class="giftcard_background">
-				<div class="wrap-90">
-					<div class="giftcard_wrap position_relative">
-						<div class="gc_centres_popup">
-							<span id="close_gc_centre">X</span>
-							<img src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/015/733/original/SOM_188_GCPlus_Website_Update_V2B.jpg?1450716958" />
+	<section id="kidsfun" class="scrollspy">
+	<div class="section_content">
+		<h1 class="margin-top-none margin-bottom-none">KIDS CLUB</h1>
+		<h1 class="margin-top-none margin-bottom-none">PROGRAMS &amp; FUN</h1>
+		<p class="whitney">SOUTHCENTRE IS ALL ABOUT YOUR TIME - HOWEVER OLD YOU ARE</p>
+		<br>
+		<div class="kidsfun_list">
+			<p class="whitney">Here are a few things we offer to make your shopping experience more enjoyable:</p>
+			<ul>
+				<li>ROAR Play Park (Kids aged 1 - 11), located on the lower level near Sport Chek. playing at ROAR is free.</li>
+				<li>Expectant Mothers / Parents with small children parking spaces.</li>
+				<li>SmarteCarte strollers for rent.</li>
+				<li>Two themed nursing rooms available and two kids play areas.</li>
+			</ul>
+		</div>
+		<div id="kid_toggle_info" style="display:none">
+			<div class="rent-box whitney">
+				<p style="margin-top:25px; font-size:1.1em">SMARTECARTE STROLLERS FOR RENT</p>
+				<p>Single and double strollers are available at both Southcentre’s main NORTH AND SOUTH ENTRANCE’S. Each stroller includes built-in tablets featuring kid’s programming and games. (This feature can be de-activated.) Strollers are offered on a first come, first served basis only.</p>
+				<p>Double $7, Single $5, $1 back when stroller is returned.</p>
+				<p style="margin-bottom:15px;"><i>Canadian cash, VISA, MasterCard and American Express cards accepted.</i></p>
+			</div>
+			<div class="kid_desc_box">
+				<div class="medium-12">
+					<div class="medium-6 columns whitney kids_desc_boxes">
+						<div class="kids_divider"></div>
+						<img alt="logo roar" class="kids_desc_img" style="width:260px; height:105px; margin-top:20px; margin-bottom:10px;" src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/009/124/original/logo_roar.jpg?1431035212" />
+						<div>
+							<p style="font-size:1.1em; margin-bottom:5px;">ANIMAL THEMED PLAY PARK</p>
+							<p class="whitney" style="font-size:1.0em; margin-bottom:30px;">
+								ROAR Play Park is built in partnership with the Calgary Zoo. Located on the lower level of the centre by Sport Chek, ROAR features all types of interactive games and activities for kids ages 1 to 11. ROAR is open during mall hours, and is unsupervised -- children cannot be left unattended. Please be sure your child has socks, as shoes must be removed while playing.
+								<a style="color:rgb(0, 174, 239);" href="/pages/southcentre-roar-play-park-rules">Click for complete rules.</a>
+							</p>
 						</div>
-						<div class="giftcard_content">
-							<h1 class="white">ONE CARD</h1>
-							<h1 class="white">SO MANY POSSIBILITIES</h1>
-							<br/>
-							<p class="freedom whitney">SHOP. STAY. FLY.</p>
-							<div class="row giftcard-description">
-								<div class="giftcard_left medium-7 columns">
-									<p class="whitney-medium all_caps">Introducing the new Southcentre Gift Card Plus!</p>
-									<p class="whitney">
-										We’ve added new benefits to our gift cards. The Southcentre Gift Card Plus is redeemable at <span class="whitney-medium gc_mobile">our centre</span><span class="whitney-medium gc_desktop" >our centre</span>, or any one of our <span class="whitney-medium"><a target="_blank" href="https://oxfordgiftcardplus.ca/where-to-use">Oxford Properties</a></span>, or can be used to book a hotel stay, or fly anywhere Porter Airlines flies.
-									</p>
-									<p class="whitney">
-										Purchase your Southcentre Gift Card Plus at Guest Services or order <span class="whitney-medium"><a target="_blank" href="https://oxfordgiftcardplus.ca/buy-gift-card/">online</a></span> and enjoy the convenience of having your gift card delivered to your door.
-									</p>
-									<p class="whitney">
-										For corporate orders please email:<br />
-										info@southcentremall.com for futher details.
-									</p>
-									<p class="whitney gc_links">
-										<a href="https://oxfordgiftcardplus.ca/" target="_blank">CORPORATE ORDERS</a><br />
-										<a href="https://oxfordgiftcardplus.ca/buy-gift-card/" target="_blank">ONLINE PURCHASES</a><br />
-										<a href="https://oxfordgiftcardplus.ca/faq/" target="_blank">FAQS</a><br />
-										<a href="https://www279.americanexpress.com/GPTHBIWeb/validateIPAction.do?clientkey=amex%20bank%20of%20canada" target="_blank">CHECK YOUR BALANCE</a><br />
-									</p>
-								</div>
-								<div class="giftcard_right medium-5 columns">
-									<img alt="gift card" src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/015/466/original/gift_card_img.png?1449786330"/>
-								</div>
-							</div>
-						</div>
+					</div>
+					<div class="medium-6 columns whitney kids_desc_boxes">
+						<!-- <div class="kids_divider"></div>  -->
+						<!-- <img  alt="kids club" class="kids_desc_img" src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/018/892/original/kidsfun.jpg?1464109213">
+							<div>
+							    <p style="font-size:1.1em; margin-bottom:5px;">KIDS CLUB</p>
+							    <p class="whitney" style="font-size:1.0em; margin-bottom:105px;">Join us the second Wednesday every month in Centre Court, 9:30am to 11:30am, for our monthly event just for the whole family! Check Facebook each month for more details. (Note: we do not host a Kids Club event in July, November or December.)</p>
+							</div> -->
+					</div>
+				</div>
+				<div class="medium-12">
+					<div class="medium-6 columns whitney kids_desc_boxes">
 					</div>
 				</div>
 			</div>
 		</div>
-	</section>
+		<div id="kid_info">
+			<div class="more_box kid_toggle_view">
+				<div class="left-div">
+					<p>MORE INFORMATION</p>
+				</div>
+				<div class="right-div">
+					<div class="kid_info-arrow"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
 </template>
 <script>
     define(["Vue"], function(Vue) {
