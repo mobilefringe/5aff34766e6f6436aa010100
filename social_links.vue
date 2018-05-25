d1a748b3a595aecd856d2e9249f9457325adb432
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-25 16:48:45 -0400

    Southcentre Mall 1.0  /social_links.vue

---
+++ social_links.vue
@@ -0,0 +1,33 @@
+<template>
+    <ul class="social_icons_list">
+								<li>
+									<a class='facebook' href="https://www.facebook.com/southcentremall" target="_blank">Facebook</a>
+								</li>
+								<li>
+									<a class='twitter' href='https://twitter.com/southcentremall' target="_blank">Twitter</a>
+								</li>
+								<li>
+									<a class='instagram' href="http://instagram.com/southcentremall/" target="_blank">Instagram</a>
+								</li>
+								<li>
+									<a class='pinterest' href='https://www.pinterest.com/southcentremall/' target="_blank">pinterest</a>
+								</li>
+								<li>
+									<a class='youtube' href='https://www.youtube.com/user/SouthcentreMall' target="_blank">youtube</a>
+								</li>
+							</ul>
+    
+</template>
+<style>
+    .lb-data .lb-number, .lb-nav {
+        display:none!important;
+    }
+</style>
+<script>
+    define(["Vue", "vuex", "lightbox"], function(Vue, Vuex, Lightbox) {
+        Vue.use(Lightbox);
+        return Vue.component("social-links", {
+            template: template, // the variable template will be injected,
+        });
+    });
+</script>
