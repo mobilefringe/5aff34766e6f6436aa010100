4715648d6e3094a85887822057be9ff4a0d16222
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-25 17:28:33 -0400

    Southcentre Mall 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -1,6 +1,36 @@
 <template>
-    <div v-if="id == 'southcentre-contact">
-        
+    <div v-if="id == 'southcentre-contact'">
+        <div class="main_content">
+          <section class="hours">
+            <div class="section_content">
+              <div class="wrap-90">
+                <h1>CONTACT</h1>
+                <h2 class="about_subheading">Southcentre is proudly managed by Oxford Properties.</h2>
+                <div class="hours_divider"></div>
+                <h3 class="contact_subheading">Guest Services</h3>
+                <p class="contact_content contact_phone">(403) 271-7670</p>
+                <div class="margin_20" style="margin-bottom:30px;"></div>
+                <h3 class="contact_subheading">Administration Office</h3>
+                <p class="contact_content contact_phone">(403) 225-9100</p>
+                <div class="hours_divider"></div>
+                <h2 class="contact_subheading">Email</h2>
+                <p class="contact_content"><a class="blue contact-email" href="mailto:info@southcentremall.com">info@southcentremall.com</a></p>
+              </div>
+            </div>
+          </section>
+          <section class="pay_it">
+            <div class="section_content">
+              <div class="wrap-90">
+                <h1>LOCATION</h1>
+                <p class="contact_content margin_0"><span class="blue">MAILING ADDRESS<a target="_blank" href="https://goo.gl/maps/nJ3xB"> / DIRECTIONS</a></span></p>
+                <p class=" contact_content margin_0 address_para">Southcentre Mall Unit 142, 100 Anderson Road SE Calgary, Alberta, Canada T2J 3V1</p>
+                <div class="map">
+                  <iframe title="directions map" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2513.352194545045!2d-114.0661583!3d50.9541923!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537176cb655cd963%3A0x841373b8b29e6a9!2s100+Anderson+Rd+SE%2C+Southcentre+Mall%2C+Calgary%2C+AB+T2J+3V1!5e0!3m2!1sen!2sca!4v1410361521688" class="map_frame" width="100%" frameborder="0" style="border:0"></iframe>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
     </div>
     <!--Pages Banner-->
 </template>
