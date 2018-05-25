5b5280d43729e4bb7076d027c130eab7e58bf4f5
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-25 16:48:48 -0400

    Southcentre Mall 1.0  /social_links.vue

--- social_links.vue
+++ social_links.vue
@@ -1,21 +1,21 @@
 <template>
     <ul class="social_icons_list">
-								<li>
-									<a class='facebook' href="https://www.facebook.com/southcentremall" target="_blank">Facebook</a>
-								</li>
-								<li>
-									<a class='twitter' href='https://twitter.com/southcentremall' target="_blank">Twitter</a>
-								</li>
-								<li>
-									<a class='instagram' href="http://instagram.com/southcentremall/" target="_blank">Instagram</a>
-								</li>
-								<li>
-									<a class='pinterest' href='https://www.pinterest.com/southcentremall/' target="_blank">pinterest</a>
-								</li>
-								<li>
-									<a class='youtube' href='https://www.youtube.com/user/SouthcentreMall' target="_blank">youtube</a>
-								</li>
-							</ul>
+		<li>
+			<a class='facebook' href="https://www.facebook.com/southcentremall" target="_blank">Facebook</a>
+		</li>
+		<li>
+			<a class='twitter' href='https://twitter.com/southcentremall' target="_blank">Twitter</a>
+		</li>
+		<li>
+			<a class='instagram' href="http://instagram.com/southcentremall/" target="_blank">Instagram</a>
+		</li>
+		<li>
+			<a class='pinterest' href='https://www.pinterest.com/southcentremall/' target="_blank">pinterest</a>
+		</li>
+		<li>
+			<a class='youtube' href='https://www.youtube.com/user/SouthcentreMall' target="_blank">youtube</a>
+		</li>
+	</ul>
     
 </template>
 <style>
