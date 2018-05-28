7268e2e3fb80a43044292546895e0de95ed9495f
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-28 16:20:00 -0400

    Southcentre Mall 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -334,7 +334,35 @@
                 </div>
             </section>
         </div>
-        <div v-if="id == 'southcentre-concierge-feedback'">
+        <div v-if="id == 'southcentre-concierge-feedback'" id="concierge-container">
+  <div class="concierge-logo"><img alt="concierge" src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/007/588/original/SOM_Tag.png?1426864225"></div>
+  <form method="post" id="subForm">
+      <div class="form-group-lg">
+          <label for="fieldName">Name</label><br />
+          <input id="fieldName" class="form-control" name="cm-name" type="text" />
+      </div>
+      <div class="form-group-lg">
+          <label for="fieldEmail">Email</label><br />
+          <input id="fieldEmail" class="form-control" name="cm-iyidlh-iyidlh" type="email" required />
+      </div>
+      <div class="form-group-lg">
+          <label for="fieldPhone">Phone number</label><br />
+          <input id="fieldPhone" class="form-control" name="cm-f-jtitlj" type="text" />
+      </div>
+      <div class="form-group-lg">
+          <label for="fieldFeedback">Feedback</label><br />
+          <textarea id="fieldFeedback" class="form-control" name="cm-f-jtitlt" type="text" /></textarea>
+      </div>
+      <div class="form-group">
+          <input id="fieldCheck" name="cm-ol-juitdh" type="checkbox" /> <label id="contact-label" for="fieldCheck">Would you like to be contacted by Mall Management regarding your feedback?</label>
+      </div>
+      <div class="form-group">
+          <button class="btn btn-default btn-lg" type="submit">Submit</button>
+      </div>
+      <div class="form-group" id="thankyou_msg">
+      </div>
+  </form>
+</div>
             
         </div>
 	    <footer-component></footer-component>
