2345d95eaa4ac2b85b31575defd63833e6bfffe0
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-28 13:46:38 -0400

    Southcentre Mall 1.0  /promotion_details.vue

--- promotion_details.vue
+++ promotion_details.vue
@@ -1,42 +1,42 @@
 <template>
     <div>
-    <div class="main_content" v-if="currentPromo" id="promo_dets_container">
-    	<section class="directory_map">
-    		<div class="section_content">
-    			<div class="directory_title">   
-    				<h1>PROMOTION DETAILS</h1>
-    			</div>
-    			<div class="promo-border">
-    				<div  class="promo_detail_image">
-    					<img :src="currentPromo.image_url">      
-    				</div>
-    				<div class="promo_detail_div">
-    					<router-link :to="'/stores/'+currentPromo.store.slug">
-    						<h3>{{currentPromo.store.name}}</h3>
-    					</router-link>
-    					<p>{{currentPromo.name}}</p>
-    					<p><strong>{{currentPromo.start_date | moment("MMM DD", timezone)}} - {{currentPromo.end_date | moment("MMM DD", timezone)}}</strong></p>
-    					<p v-html="currentPromo.rich_description"></p>
-    				</div>
-    				<div class="clearfix"></div>
-    			</div>
-    		</div>
-    	</section>
-    	<section class="directory_store_description" v-if="currentPromo.description">
-    		<div class="section_content">
-    			<div class="wrap-90">
-    				<div class="directory-details">
-    					<p class="blue-title">Promotion Description</p>
-    					<div class="hours_divider"></div>
-    					<div class="store-description">
-    						<p>{{currentPromo.description}}</p>
-    					</div>
-    					<div class="hours_divider"></div>
-    				</div>
-    			</div>
-    		</div>
-    	</section>
-    </div>
+        <div class="main_content" v-if="currentPromo">
+        	<section class="directory_map">
+        		<div class="section_content">
+        			<div class="directory_title">   
+        				<h1>PROMOTION DETAILS</h1>
+        			</div>
+        			<div class="promo-border">
+        				<div  class="promo_detail_image">
+        					<img :src="currentPromo.image_url">      
+        				</div>
+        				<div class="promo_detail_div">
+        					<router-link :to="'/stores/'+currentPromo.store.slug">
+        						<h3>{{currentPromo.store.name}}</h3>
+        					</router-link>
+        					<p>{{currentPromo.name}}</p>
+        					<p><strong>{{currentPromo.start_date | moment("MMM DD", timezone)}} - {{currentPromo.end_date | moment("MMM DD", timezone)}}</strong></p>
+        					<p v-html="currentPromo.rich_description"></p>
+        				</div>
+        				<div class="clearfix"></div>
+        			</div>
+        		</div>
+        	</section>
+        	<section class="directory_store_description" v-if="currentPromo.description">
+        		<div class="section_content">
+        			<div class="wrap-90">
+        				<div class="directory-details">
+        					<p class="blue-title">Promotion Description</p>
+        					<div class="hours_divider"></div>
+        					<div class="store-description">
+        						<p>{{currentPromo.description}}</p>
+        					</div>
+        					<div class="hours_divider"></div>
+        				</div>
+        			</div>
+        		</div>
+        	</section>
+        </div>
     </div>
 </template>
 <style>
