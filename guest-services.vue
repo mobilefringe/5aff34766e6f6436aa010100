7928b24bc0e380db21d1d361877a2bd9341eb7c0
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-28 10:22:16 -0400

    Southcentre Mall 1.0  /guest-services.vue

--- guest-services.vue
+++ guest-services.vue
@@ -46,7 +46,7 @@
 </template>
 <script>
     define(["Vue"], function(Vue) {
-        return Vue.component("giftcard-component", {
+        return Vue.component("guest-services-component", {
             template: template, // the variable template will be injected,
         });
     });
