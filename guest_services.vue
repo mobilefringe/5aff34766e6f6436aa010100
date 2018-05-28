391ef8d653ec40a938e7649aa4ac0c0557b96538
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-28 10:24:04 -0400

    Southcentre Mall 1.0  /guest_services.vue

---
+++ guest_services.vue
@@ -0,0 +1,88 @@
+<template>
+	<footer>
+		<section class="footer">
+			<div class="section_content">
+				<div class="footer_background">
+					<div class="footer_wrap">
+						<div class="footer_content">
+							<h1 class="black">You can reach Guest Services at:<br/><span class="chronicle-display-italic black">(403) 271-7670</span></h1>
+							<ul class="social_icons_list">
+								<li>
+									<a class='facebook' href="https://www.facebook.com/southcentremall" target="_blank">FB</a>
+								</li>
+								<li>
+									<a class='twitter' href='https://twitter.com/southcentremall' target="_blank">twitter</a>
+								</li>
+								<li>
+									<a class='instagram' href="http://instagram.com/southcentre" target="_blank">insta</a>
+								</li>
+								<li>
+									<a class='pinterest' href='https://www.pinterest.com/southcentremall/' target="_blank">pinterest</a>
+								</li>
+								<li>
+									<a class='youtube' href='https://www.youtube.com/user/SouthcentreMall' target="_blank">youtube</a>
+								</li>
+							</ul>
+							<h2 class="black chronicle">CORNER<em>&nbsp;&nbsp;of&nbsp;&nbsp;</em>ANDERSON<em>&nbsp;&nbsp;&&nbsp;&nbsp;</em>MACLEOD</h2>
+							<hr>
+							<div class="footer_nav">
+								<ul class="footer_links">
+									<li><a href="/pages/southcentre-about-us">About Us</a></li>
+									<li><a href="/pages/southcentre-contact">Contact</a></li>
+									<li><a href="/jobs">Careers</a></li>
+									<li><a href="/pages/southcentre-leasing">Leasing</a></li>
+									<li><a href="/pages/southcentre-media">Media</a></li>
+									<li><a href="/retail_users/sign_in">Retailer Login</a></li>
+									<li><a href="http://www.oxfordproperties.com/corp/downloads/Oxford_Privacy_Statement-EN_2017.pdf">Privacy Statement</a></li>
+								</ul>
+								<img  alt="logo oxford" src="https://www.mallmaverick.com/system/site_images/photos/000/023/729/original/logo_oxford_black.png?1479508111"/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</footer>
+</template>
+<style>
+	footer td,footer td label {
+    	font-family: WhitneyHTF-Book !important;
+    	font-size: 1.1383333467168em !important;
+	}
+	footer tr {
+    	background:none !important;
+    	vertical-align: top;
+	}
+	footer table{
+    	background:none !important;
+    	border: 0px !important;
+	}
+	footer .agreementLabel{
+	    vertical-align: top;
+	}
+	footer .newsletter_agreement {
+	    margin-left: 10px;
+	}
+	footer .error{color:#F00}
+	@media (max-width:768px){
+    	footer .agreementLabel{
+    	    vertical-align: none !important;
+    	} 
+	}
+	@media only screen and (max-width: 120.063em) and (min-width: 64.063em) {
+        footer  h1 {
+            font-size: 3rem;
+        }
+        .footer_content h2 {
+            font-size: 1.2rem;
+        }
+    }
+
+</style>
+<script>
+    define(["Vue"], function(Vue) {
+        return Vue.component("guest-services-component", {
+            template: template, // the variable template will be injected,
+        });
+    });
+</script>
