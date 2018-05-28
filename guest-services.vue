78539c45354f30f124e213af91fc78a082406de2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-28 10:21:54 -0400

    Southcentre Mall 1.0  /guest-services.vue

--- guest-services.vue
+++ guest-services.vue
@@ -1,48 +1,48 @@
 <template>
 	<footer>
-        <section class="footer">
-            <div class="section_content">
-              <div class="footer_background">
-                <div class="footer_wrap">
-                  <div class="footer_content">
-                    <h1 class="black">You can reach Guest Services at:<br/><span class="chronicle-display-italic black">(403) 271-7670</span></h1>
-                    <ul class="social_icons_list">
-                      <li>
-                    <a class='facebook' href="https://www.facebook.com/southcentremall" target="_blank">FB</a>
-                  </li>
-                  <li>
-                    <a class='twitter' href='https://twitter.com/southcentremall' target="_blank">twitter</a>
-                  </li>
-                  <li>
-                    <a class='instagram' href="http://instagram.com/southcentre" target="_blank">insta</a>
-                  </li>
-                  <li>
-                    <a class='pinterest' href='https://www.pinterest.com/southcentremall/' target="_blank">pinterest</a>
-                  </li>
-                  <li>
-                    <a class='youtube' href='https://www.youtube.com/user/SouthcentreMall' target="_blank">youtube</a>
-                  </li>
-                    </ul>
-                    <h2 class="black chronicle">CORNER<em>&nbsp;&nbsp;of&nbsp;&nbsp;</em>ANDERSON<em>&nbsp;&nbsp;&&nbsp;&nbsp;</em>MACLEOD</h2>
-                    <hr>
-                    <div class="footer_nav">
-                        <ul class="footer_links">
-                          <li><a href="/pages/southcentre-about-us">About Us</a></li>
-                          <li><a href="/pages/southcentre-contact">Contact</a></li>
-                          <li><a href="/jobs">Careers</a></li>
-                          <li><a href="/pages/southcentre-leasing">Leasing</a></li>
-                          <li><a href="/pages/southcentre-media">Media</a></li>
-                          <li><a href="/retail_users/sign_in">Retailer Login</a></li>
-                          <li><a href="http://www.oxfordproperties.com/corp/downloads/Oxford_Privacy_Statement-EN_2017.pdf">Privacy Statement</a></li>
-                        </ul>
-                        <img  alt="logo oxford" src="https://www.mallmaverick.com/system/site_images/photos/000/023/729/original/logo_oxford_black.png?1479508111"/>
-                    </div>                  
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
-        </footer>
+		<section class="footer">
+			<div class="section_content">
+				<div class="footer_background">
+					<div class="footer_wrap">
+						<div class="footer_content">
+							<h1 class="black">You can reach Guest Services at:<br/><span class="chronicle-display-italic black">(403) 271-7670</span></h1>
+							<ul class="social_icons_list">
+								<li>
+									<a class='facebook' href="https://www.facebook.com/southcentremall" target="_blank">FB</a>
+								</li>
+								<li>
+									<a class='twitter' href='https://twitter.com/southcentremall' target="_blank">twitter</a>
+								</li>
+								<li>
+									<a class='instagram' href="http://instagram.com/southcentre" target="_blank">insta</a>
+								</li>
+								<li>
+									<a class='pinterest' href='https://www.pinterest.com/southcentremall/' target="_blank">pinterest</a>
+								</li>
+								<li>
+									<a class='youtube' href='https://www.youtube.com/user/SouthcentreMall' target="_blank">youtube</a>
+								</li>
+							</ul>
+							<h2 class="black chronicle">CORNER<em>&nbsp;&nbsp;of&nbsp;&nbsp;</em>ANDERSON<em>&nbsp;&nbsp;&&nbsp;&nbsp;</em>MACLEOD</h2>
+							<hr>
+							<div class="footer_nav">
+								<ul class="footer_links">
+									<li><a href="/pages/southcentre-about-us">About Us</a></li>
+									<li><a href="/pages/southcentre-contact">Contact</a></li>
+									<li><a href="/jobs">Careers</a></li>
+									<li><a href="/pages/southcentre-leasing">Leasing</a></li>
+									<li><a href="/pages/southcentre-media">Media</a></li>
+									<li><a href="/retail_users/sign_in">Retailer Login</a></li>
+									<li><a href="http://www.oxfordproperties.com/corp/downloads/Oxford_Privacy_Statement-EN_2017.pdf">Privacy Statement</a></li>
+								</ul>
+								<img  alt="logo oxford" src="https://www.mallmaverick.com/system/site_images/photos/000/023/729/original/logo_oxford_black.png?1479508111"/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</footer>
 </template>
 <script>
     define(["Vue"], function(Vue) {
