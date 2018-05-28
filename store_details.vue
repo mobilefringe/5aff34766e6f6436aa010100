37140b533d4d1fb76516aa6d638e4c6262f1f2ae
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-28 11:55:51 -0400

    Southcentre Mall 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -107,7 +107,7 @@
     </div>
 </template>
 <script>
-    define(['Vue', 'vuex', 'moment'], function(Vue, Vuex, moment) {
+    define(['Vue', 'vuex', 'moment',"vue!mapplic-png-map"], function(Vue, Vuex, moment, MapplicComponent) {
         return Vue.component("store-details-component", {
             template: template, // the variable template will be injected,
             data: function() {
