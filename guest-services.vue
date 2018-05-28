be6b8a7e8b5935173468c338b98e5a445fe115e9
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-28 10:21:36 -0400

    Southcentre Mall 1.0  /guest-services.vue

--- guest-services.vue
+++ guest-services.vue
@@ -1,48 +1,48 @@
 <template>
-	<section id="giftcard" class="scrollspy">
-		<div class="section_content">
-			<div class="giftcard_background">
-				<div class="wrap-90">
-					<div class="giftcard_wrap position_relative">
-						<div class="gc_centres_popup">
-							<span id="close_gc_centre">X</span>
-							<img src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/015/733/original/SOM_188_GCPlus_Website_Update_V2B.jpg?1450716958" />
-						</div>
-						<div class="giftcard_content">
-							<h1 class="white">ONE CARD</h1>
-							<h1 class="white">SO MANY POSSIBILITIES</h1>
-							<br/>
-							<p class="freedom whitney">SHOP. STAY. FLY.</p>
-							<div class="row giftcard-description">
-								<div class="giftcard_left medium-7 columns">
-									<p class="whitney-medium all_caps">Introducing the new Southcentre Gift Card Plus!</p>
-									<p class="whitney">
-										We’ve added new benefits to our gift cards. The Southcentre Gift Card Plus is redeemable at <span class="whitney-medium gc_mobile">our centre</span><span class="whitney-medium gc_desktop" >our centre</span>, or any one of our <span class="whitney-medium"><a target="_blank" href="https://oxfordgiftcardplus.ca/where-to-use">Oxford Properties</a></span>, or can be used to book a hotel stay, or fly anywhere Porter Airlines flies.
-									</p>
-									<p class="whitney">
-										Purchase your Southcentre Gift Card Plus at Guest Services or order <span class="whitney-medium"><a target="_blank" href="https://oxfordgiftcardplus.ca/buy-gift-card/">online</a></span> and enjoy the convenience of having your gift card delivered to your door.
-									</p>
-									<p class="whitney">
-										For corporate orders please email:<br />
-										info@southcentremall.com for futher details.
-									</p>
-									<p class="whitney gc_links">
-										<a href="https://oxfordgiftcardplus.ca/" target="_blank">CORPORATE ORDERS</a><br />
-										<a href="https://oxfordgiftcardplus.ca/buy-gift-card/" target="_blank">ONLINE PURCHASES</a><br />
-										<a href="https://oxfordgiftcardplus.ca/faq/" target="_blank">FAQS</a><br />
-										<a href="https://www279.americanexpress.com/GPTHBIWeb/validateIPAction.do?clientkey=amex%20bank%20of%20canada" target="_blank">CHECK YOUR BALANCE</a><br />
-									</p>
-								</div>
-								<div class="giftcard_right medium-5 columns">
-									<img alt="gift card" src="//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/015/466/original/gift_card_img.png?1449786330"/>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+	<footer>
+        <section class="footer">
+            <div class="section_content">
+              <div class="footer_background">
+                <div class="footer_wrap">
+                  <div class="footer_content">
+                    <h1 class="black">You can reach Guest Services at:<br/><span class="chronicle-display-italic black">(403) 271-7670</span></h1>
+                    <ul class="social_icons_list">
+                      <li>
+                    <a class='facebook' href="https://www.facebook.com/southcentremall" target="_blank">FB</a>
+                  </li>
+                  <li>
+                    <a class='twitter' href='https://twitter.com/southcentremall' target="_blank">twitter</a>
+                  </li>
+                  <li>
+                    <a class='instagram' href="http://instagram.com/southcentre" target="_blank">insta</a>
+                  </li>
+                  <li>
+                    <a class='pinterest' href='https://www.pinterest.com/southcentremall/' target="_blank">pinterest</a>
+                  </li>
+                  <li>
+                    <a class='youtube' href='https://www.youtube.com/user/SouthcentreMall' target="_blank">youtube</a>
+                  </li>
+                    </ul>
+                    <h2 class="black chronicle">CORNER<em>&nbsp;&nbsp;of&nbsp;&nbsp;</em>ANDERSON<em>&nbsp;&nbsp;&&nbsp;&nbsp;</em>MACLEOD</h2>
+                    <hr>
+                    <div class="footer_nav">
+                        <ul class="footer_links">
+                          <li><a href="/pages/southcentre-about-us">About Us</a></li>
+                          <li><a href="/pages/southcentre-contact">Contact</a></li>
+                          <li><a href="/jobs">Careers</a></li>
+                          <li><a href="/pages/southcentre-leasing">Leasing</a></li>
+                          <li><a href="/pages/southcentre-media">Media</a></li>
+                          <li><a href="/retail_users/sign_in">Retailer Login</a></li>
+                          <li><a href="http://www.oxfordproperties.com/corp/downloads/Oxford_Privacy_Statement-EN_2017.pdf">Privacy Statement</a></li>
+                        </ul>
+                        <img  alt="logo oxford" src="https://www.mallmaverick.com/system/site_images/photos/000/023/729/original/logo_oxford_black.png?1479508111"/>
+                    </div>                  
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </footer>
 </template>
 <script>
     define(["Vue"], function(Vue) {
