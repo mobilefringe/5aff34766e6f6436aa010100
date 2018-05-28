6ba7f469f2b99846abbe83af476d1d675dd11bda
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-28 11:56:19 -0400

    Southcentre Mall 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -201,6 +201,24 @@
                 pngMapRef() {
                     return this.$refs.pngmapref;
                 },
+                allStores() {
+                    this.processedStores.map(function(store){
+                        store.zoom = 1;
+                    })
+                    return this.processedStores;
+                },
+                floorList () {
+                    var floor_list = [];
+                    
+                    var floor_1 = {};
+                    floor_1.id = "first-floor";
+                    floor_1.title = "Level One";
+                    floor_1.map = this.getPNGurl;
+                    floor_1.z_index = 1;
+                    floor_1.show = true;
+                    floor_list.push(floor_1);
+                    return floor_list;
+                }
             },
             methods: {
                 loadData: async function() {
