7e4cc4425f17703dc5be334b3b1190bc92836c60
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-28 13:37:03 -0400

    Southcentre Mall 1.0  /promotion_details.vue

---
+++ promotion_details.vue
@@ -0,0 +1,157 @@
+<template>
+	<div class="main_content" v-if="currentPromo">
+        <section class="directory_map">
+          <div class="section_content">
+            <div class="directory_title">
+            {% if promo_nav == "true" %}    
+              <h1>PROMOTION DETAILS</h1>
+            {% else %}
+              <h1>EVENT DETAILS</h1>
+            {% endif %}
+            </div>
+            <div class="promo-border">
+                <div  class="promo_detail_image">
+                {% if event.promo_image_path  contains "missing.png"%}
+                    <img src ="/system/site_images/photos/000/003/856/original/logo_sc_desktop.png?1410203855" >
+                {% else %}
+                    <img src="{{event.promo_image_path}}">      
+                {% endif%}
+                </div>
+                <div class="promo_detail_div">
+                    <a href="/stores/{{event.promotionable.slug}}"><h3>{{event.promotionable.name}}</h3></a>
+                    <p>{{event.name}}</p>
+                    <p><strong>{{ event.start_date | get_date }} - {{ event.end_date | get_date }}</strong></p>
+                    <p>{{event.description}}</p>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+          </div>
+        </section>
+        {% if promo.description %}
+        <section class="directory_store_description">
+          <div class="section_content">
+            <div class="wrap-90">
+              <div class="directory-details">
+                <p class="blue-title">Promotion Description</p>
+                <div class="hours_divider"></div>
+                  <div class="store-description">
+                    <p>{{promo.description}}</p>
+                  </div>
+                <div class="hours_divider"></div>
+              </div>
+            </div>
+          </div>
+        </section>
+        {% endif %}
+      </div>
+</template>
+
+<script>
+    define(['Vue', 'vuex', 'moment', 'vue-lazy-load'], function(Vue, Vuex, moment, VueLazyload) {
+        Vue.use(VueLazyload);
+        return Vue.component("promo-details-component", {
+            template: template, // the variable template will be injected,
+            props:['id', 'locale'],
+            data: function() {
+                return {
+                    currentPromo: null,
+                    storePromos : null,
+                    storeHours : null,
+                    promoBanner : null
+                }
+            },
+            beforeRouteUpdate(to, from, next) {
+                this.currentPromo = this.findPromoBySlug(to.params.id);
+                    if (this.currentPromo === null || this.currentPromo === undefined){
+                        this.$router.replace('/');
+                    }
+                next();
+            },
+            created(){
+                this.loadData().then(response => {
+                    this.updateCurrentPromo(this.id);
+                    var temp_repo = this.findRepoByName('Promos Banner');
+                    if(temp_repo) {
+                        this.promoBanner = temp_repo.images[0];
+                    }
+                    console.log(this.promoBanner);
+                    this.promos = this.promotions;
+                });
+            },
+            watch: {
+                currentPromo : function (){
+                    if(this.currentPromo != null) {
+                        console.log(this.currentPromo.store);
+                        if (this.currentPromo.store != null && this.currentPromo.store != undefined && _.includes(this.currentPromo.store.image_url, 'missing')) {
+                            this.currentPromo.store.image_url = "//codecloud.cdn.speedyrails.net/sites/5a6a54eb6e6f647da51e0100/image/png/1518554684072/bonniedoonlogo.png";
+                        }
+                        else if (this.currentPromo.store == null || this.currentPromo.store == undefined) {
+                            this.currentPromo.store = {};
+                            this.currentPromo.store.image_url =  "//codecloud.cdn.speedyrails.net/sites/5a6a54eb6e6f647da51e0100/image/png/1518554684072/bonniedoonlogo.png";
+                        }
+                        var vm = this;
+                        var temp_promo = [];
+                        var current_id =_.toNumber(this.currentPromo.id);
+                        _.forEach(this.currentPromo.store.promotions, function(value, key) {
+                            if(_.toNumber(value) != current_id){
+                                var current_promo = vm.findPromoById(value);
+                                current_promo.description_short = _.truncate(current_promo.description, {'length': 70});
+                                temp_promo.push(current_promo);
+                            }
+                        });
+                        this.storePromos = temp_promo;
+                    }
+                    if(this.currentPromo.store) {
+                        var storeHours = [];
+                        var vm = this;
+                        _.forEach(this.currentPromo.store.store_hours, function (value, key) {
+                            var hour = vm.findHourById(value);
+                            if(hour.day_of_week === 0){
+                                hour.order = 7;
+                            }
+                            else {
+                                hour.order = hour.day_of_week;
+                            }
+                            storeHours.push();
+                        });
+                        this.storeHours = _.sortBy(storeHours, [function(o) { return o.order; }]);
+                    }
+                }
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'processedPromos',
+                    'findPromoBySlug',
+                    'findPromoById',
+                    'timezone',
+                    'findRepoByName',
+                    'findHourById'
+                ]),
+                allPromos() {
+                    return this.processedPromos;
+                },
+            },
+            methods: {
+                updateCurrentPromo (id) {
+                    this.currentPromo = this.findPromoBySlug(id);
+                    if (this.currentPromo === null || this.currentPromo === undefined){
+                        this.$router.replace('/');
+                    }
+                },
+                loadData: async function() {
+                    try {
+                        // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
+                        let results = await Promise.all([this.$store.dispatch("getData", "promotions"), this.$store.dispatch("getData", "repos")]);
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                },
+                shareURL(slug){
+                    var share_url = "http://mallmaverick.ca/promotions/" + slug;
+                    return share_url;
+                },
+            }
+        });
+    });
+</script>
