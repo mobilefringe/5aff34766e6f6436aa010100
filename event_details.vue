b3d95a1a3210c6535761abc29196b67d38b4f476
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-28 13:50:06 -0400

    Southcentre Mall 1.0  /event_details.vue

---
+++ event_details.vue
@@ -0,0 +1,152 @@
+<template>
+<div id="promo_dets_container">
+        <div class="main_content" v-if="currentPromo">
+        	<section class="directory_map">
+        		<div class="section_content">
+        			<div class="directory_title">   
+        				<h1>PROMOTION DETAILS</h1>
+        			</div>
+        			<div class="promo-border">
+        				<div  class="promo_detail_image">
+        					<img :src="currentPromo.image_url">      
+        				</div>
+        				<div class="promo_detail_div">
+        					<router-link :to="'/stores/'+currentPromo.store.slug">
+        						<h3>{{currentPromo.store.name}}</h3>
+        					</router-link>
+        					<p>{{currentPromo.name}}</p>
+        					<p><strong>{{currentPromo.start_date | moment("MMM DD", timezone)}} - {{currentPromo.end_date | moment("MMM DD", timezone)}}</strong></p>
+        					<p v-html="currentPromo.rich_description"></p>
+        				</div>
+        				<div class="clearfix"></div>
+        			</div>
+        		</div>
+        	</section>
+        	<section class="directory_store_description" v-if="currentPromo.description">
+        		<div class="section_content">
+        			<div class="wrap-90">
+        				<div class="directory-details">
+        					<p class="blue-title">Promotion Description</p>
+        					<div class="hours_divider"></div>
+        					<div class="store-description">
+        						<p>{{currentPromo.description}}</p>
+        					</div>
+        					<div class="hours_divider"></div>
+        				</div>
+        			</div>
+        		</div>
+        	</section>
+        </div>
+        <guest-services-component></guest-services-component>
+    </div>
+</template>
+
+<script>
+    define(['Vue', 'vuex', 'moment', 'vue-lazy-load'], function(Vue, Vuex, moment, VueLazyload) {
+        Vue.use(VueLazyload);
+        return Vue.component("event-details-component", {
+            template: template, // the variable template will be injected,
+            data: function() {
+                return {
+                    currentEvent: null,
+                    storeEvents : null,
+                    storeHours : null,
+                    eventBanner : null
+                }
+            },
+            props:['id', 'locale'],
+            beforeRouteUpdate(to, from, next) {
+                this.currentEvent = this.findEventBySlug(to.params.id);
+                    if (this.currentEvent === null || this.currentEvent === undefined){
+                        this.$router.replace('/');
+                    }
+                next();
+            },
+            created(){
+                this.loadData().then(response => {
+                    this.updatecurrentEvent(this.id);
+                    var temp_repo = this.findRepoByName('Events Banner');
+                    if(temp_repo) {
+                        this.eventBanner = temp_repo.images[0];
+                    }
+                    console.log(this.eventBanner);
+                    this.events = this.event;
+                });
+            },
+            watch: {
+                currentEvent : function (){
+                    if(this.currentEvent != null) {
+                        console.log(this.currentEvent.store);
+                        if (this.currentEvent.store != null && this.currentEvent.store != undefined && _.includes(this.currentEvent.store.image_url, 'missing')) {
+                            this.currentEvent.store.image_url = "//codecloud.cdn.speedyrails.net/sites/5a6a54eb6e6f647da51e0100/image/png/1518554684072/bonniedoonlogo.png";
+                        }
+                        else if (this.currentEvent.store == null || this.currentEvent.store == undefined) {
+                            this.currentEvent.store = {};
+                            this.currentEvent.store.image_url =  "//codecloud.cdn.speedyrails.net/sites/5a6a54eb6e6f647da51e0100/image/png/1518554684072/bonniedoonlogo.png";
+                        }
+                        var vm = this;
+                        var temp_event = [];
+                        var current_id =_.toNumber(this.currentEvent.id);
+                        _.forEach(this.currentEvent.store.event, function(value, key) {
+                            if(_.toNumber(value) != current_id){
+                                var current_event = vm.findEventById(value);
+                                current_event.description_short = _.truncate(current_event.description, {'length': 70});
+                                temp_event.push(current_event);
+                            }
+                        });
+                        this.storeEvents = temp_event;
+                    }
+                    if(this.currentEvent.store) {
+                        var storeHours = [];
+                        var vm = this;
+                        _.forEach(this.currentEvent.store.store_hours, function (value, key) {
+                            var hour = vm.findHourById(value);
+                            if(hour.day_of_week === 0){
+                                hour.order = 7;
+                            }
+                            else {
+                                hour.order = hour.day_of_week;
+                            }
+                            storeHours.push();
+                        });
+                        this.storeHours = _.sortBy(storeHours, [function(o) { return o.order; }]);;
+                    }
+                }
+            },
+            computed: {
+                ...Vuex.mapGetters([
+                    'property',
+                    'processedEents',
+                    'findEventBySlug',
+                    'findEventById',
+                    'timezone',
+                    'findRepoByName',
+                    'findHourById'
+                ]),
+                allEvents() {
+                    return this.processedEvents;
+                },
+            },
+            methods: {
+                updatecurrentEvent (id) {
+                    this.currentEvent = this.findEventBySlug(id);
+                    if (this.currentEvent === null || this.currentEvent === undefined){
+                        this.$router.replace('/');
+                    }
+                },
+                loadData: async function() {
+                    try {
+                        // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
+                        let results = await Promise.all([this.$store.dispatch("getData", "events"), this.$store.dispatch("getData", "repos")]);
+                    } catch (e) {
+                        console.log("Error loading data: " + e.message);
+                    }
+                },
+                shareURL(slug){
+                    var share_url = "http://mallmaverick.ca/event/" + slug;
+                    return share_url;
+                },
+            }
+        });
+    });
+</script>
