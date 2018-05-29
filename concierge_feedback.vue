e4b10f6e40b41109b840d60553846ae2d2a68a0e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-29 18:21:15 -0400

    Southcentre Mall 1.0  /concierge_feedback.vue

--- concierge_feedback.vue
+++ concierge_feedback.vue
@@ -99,29 +99,29 @@
         return Vue.component("concierge-feedback", {
             template: template, // the variable template will be injected,
             methods: {
-                 // submitForm() {
-                //     e.preventDefault();
-                //     var checkbox = "NO";
-                //     if ($("#fieldCheck").is(':checked')) {
-                //       checkbox = "YES";
-                //     };
-                //     var data = {
-                //         name: $("#fieldName").val(),
-                //         email: $("#fieldEmail").val(),
-                //         phone: $("#fieldPhone").val(),
-                //         feedback: $("#fieldFeedback").val(),
-                //         agree_to_contact: checkbox
-                //     };
-                //     $.ajax({
-                //         type: "POST",
-                //         url: "//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/008/177/original/email.php?1428439361",
-                //         data: data,
-                //         success: function(){
-                //           alert("Thank you for your feedback!");
-                //           document.getElementById("subForm").reset();
-                //         }
-                //     });
-                // }
+                  submitForm() {
+                    e.preventDefault();
+                    var checkbox = "NO";
+                    if ($("#fieldCheck").is(':checked')) {
+                      checkbox = "YES";
+                    };
+                    var data = {
+                        name: $("#fieldName").val(),
+                        email: $("#fieldEmail").val(),
+                        phone: $("#fieldPhone").val(),
+                        feedback: $("#fieldFeedback").val(),
+                        agree_to_contact: checkbox
+                    };
+                    $.ajax({
+                        type: "POST",
+                        url: "//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/008/177/original/email.php?1428439361",
+                        data: data,
+                        success: function(){
+                          alert("Thank you for your feedback!");
+                          document.getElementById("subForm").reset();
+                        }
+                    });
+                }
             }
         });
     });
