0370bd278705dff8464f0906a9865ee4f888c67d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-29 18:14:06 -0400

    Southcentre Mall 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -352,10 +352,9 @@
         margin-top: 100px;
     }
     
-    
 </style>
 <script>
-    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "vue!footer", 'jquery', 'sweet-alert'], function(Vue, Vuex, moment, tz, VueMoment, footerComponent, $, sweetAlert) {
+    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "vue!footer"], function(Vue, Vuex, moment, tz, VueMoment, footerComponent) {
         return Vue.component("page-details-component", {
             template: template, // the variable template will be injected,
             data: function() {
