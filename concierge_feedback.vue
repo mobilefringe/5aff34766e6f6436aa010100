950ae56c42a4a1162b6e8023d73ca44193cdff97
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-29 18:21:21 -0400

    Southcentre Mall 1.0  /concierge_feedback.vue

--- concierge_feedback.vue
+++ concierge_feedback.vue
@@ -112,15 +112,15 @@
                         feedback: $("#fieldFeedback").val(),
                         agree_to_contact: checkbox
                     };
-                    $.ajax({
-                        type: "POST",
-                        url: "//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/008/177/original/email.php?1428439361",
-                        data: data,
-                        success: function(){
-                          alert("Thank you for your feedback!");
-                          document.getElementById("subForm").reset();
-                        }
-                    });
+                    // $.ajax({
+                    //     type: "POST",
+                    //     url: "//mallmaverick.cdn.speedyrails.net/system/site_images/photos/000/008/177/original/email.php?1428439361",
+                    //     data: data,
+                    //     success: function(){
+                    //       alert("Thank you for your feedback!");
+                    //       document.getElementById("subForm").reset();
+                    //     }
+                    // });
                 }
             }
         });
