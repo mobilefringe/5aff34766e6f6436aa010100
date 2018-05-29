f71cc00f0ccc2a287fb76ad9bb87a9fe92e38624
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-29 12:42:45 -0400

    Southcentre Mall 1.0  /guest_services.vue

--- guest_services.vue
+++ guest_services.vue
@@ -5,7 +5,7 @@
 				<div class="footer_background">
 					<div class="footer_wrap">
 						<div class="footer_content">
-						<hr>
+						    <!--<hr>-->
 							<h1 class="black">You can reach Guest Services at:<br/><span class="chronicle-display-italic black">(403) 271-7670</span></h1>
 							<social-links></social-links>
 							<h2 class="black chronicle">CORNER<em>&nbsp;&nbsp;of&nbsp;&nbsp;</em>ANDERSON<em>&nbsp;&nbsp;&&nbsp;&nbsp;</em>MACLEOD</h2>
