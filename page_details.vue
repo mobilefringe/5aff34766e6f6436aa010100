6ba9a62be6bcb1eb2d5c99ef9787dffadc23ddba
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-30 10:19:33 -0400

    Southcentre Mall 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -398,7 +398,7 @@
     }
 </style>
 <script>
-    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "lightbox","vue!footer", "vue!concierge_feedback",  "vue!concierge"], function(Vue, Vuex, moment, tz, VueMoment, lightbox, footerComponent, ConciergeFeedback, ConciergeComponent) {
+    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "lightbox","vue!footer", "vue!concierge_feedback",  "vue!concierge"], function(Vue, Vuex, moment, tz, VueMoment, Lightbox, footerComponent, ConciergeFeedback, ConciergeComponent) {
         return Vue.component("page-details-component", {
             template: template, // the variable template will be injected,
             data: function() {
