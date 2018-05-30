bccef0bce36cce19e42da0387834ce23d1514ba7
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-30 10:20:49 -0400

    Southcentre Mall 1.0  /page_details.vue

--- page_details.vue
+++ page_details.vue
@@ -352,7 +352,7 @@
                 </div>
             </section>
         </div>
-        <div class="main_content" v-if="id == 'southcentre-concierge'">
+        <div class="main_content" v-if="id == 'southcentre-prices'">
             <div style="max-width:1040px; margin:0 auto;">
                 <img style="width:100%" src="https://www.mallmaverick.com/system/site_images/photos/000/023/570/original/SOM_217_SantaSignage_PhotoPrices_22x28_FA.jpg?1479325165" />
             </div>
