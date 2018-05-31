f39f3f0530f9961f206ca76b946a43690377a1a3
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-05-31 10:44:40 -0400

    Southcentre Mall 1.0  /stores_map_png.vue

--- stores_map_png.vue
+++ stores_map_png.vue
@@ -1,6 +1,6 @@
 <template>
   <div style="position:relative; margin: auto;width: 100%;overflow: hidden;">
-    <div id="mapplic" class="mapplic"  ></div>
+    <div id="mapplic" class="mapplic" v-if="show_map"></div>
 </div>
 </template>
 
